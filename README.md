412e69920248b812498da262c720333092415c99
Sergei Litvinenko <sv.litvinenko@outlook.com>
2020-07-04 12:55:51 +0300

    Initial Commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# Note. Kata Project — 1
+
+Ката по туториалу [ResoCoder](https://www.youtube.com/playlist?list=PLB6lc7nQ1n4iS5p-IezFFgqP6YvAJy84U).
---
+++ android/app/src/main/kotlin/com/example/kata_note_flutter/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.kata_note_flutter
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
