af476b1beefdccbe14605d5e19752da8eca9686a
Sergei Litvinenko <sv.litvinenko@outlook.com>
2020-07-17 10:42:30 +0300

    #21. Note Value Objects
    Resolves #21.

--- lib/domain/core/value_validators.dart
+++ lib/domain/core/value_validators.dart
@@ -1,5 +1,6 @@
 import 'package:dartz/dartz.dart';
 import 'package:kata_note_flutter/domain/core/failures.dart';
+import 'package:kt_dart/collection.dart';
 
 Either<ValueFailure<String>, String> validateEmailAddress(String input) {
   const regex = r""".+@.+\..+""";
@@ -11,10 +12,42 @@ Either<ValueFailure<String>, String> validateEmailAddress(String input) {
   }
 }
 
-Either<ValueFailure<String>, String> validatePassord(String input) {
+Either<ValueFailure<String>, String> validatePassword(String input) {
   if (input.length >= 8) {
     return right(input);
   } else {
     return left(ValueFailure.shortPassword(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validateMaxStringLength(String input, int maxLength) {
+  if (input.length <= maxLength) {
+    return right(input);
+  } else {
+    return left(ValueFailure.exceedingLength(failedValue: input, maxPermitted: maxLength));
+  }
+}
+
+Either<ValueFailure<String>, String> validateStringNotEmpty(String input) {
+  if (input.isNotEmpty) {
+    return right(input);
+  } else {
+    return left(ValueFailure.empty(failedValue: input));
+  }
+}
+
+Either<ValueFailure<String>, String> validateStringIsSingleline(String input) {
+  if (input.contains('\n')) {
+    return left(ValueFailure.empty(failedValue: input));
+  } else {
+    return right(input);
+  }
+}
+
+Either<ValueFailure<KtList<T>>, KtList<T>> validateMaxListLength<T>(KtList<T> input, int maxLength) {
+  if (input.size <= maxLength) {
+    return right(input);
+  } else {
+    return left(ValueFailure.listTooLong(failedValue: input, maxPermitted: maxLength));
   }
 }
