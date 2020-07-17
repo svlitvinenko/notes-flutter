87dbe2da4859be2cc8bdb54e0329824dadce2d9f
Sergei Litvinenko <sv.litvinenko@outlook.com>
2020-07-17 20:00:10 +0300

    #35. Notes Overview Page
    Resolves #35.

--- lib/presentation/routes/router.dart
+++ lib/presentation/routes/router.dart
@@ -1,5 +1,6 @@
 import 'package:auto_route/auto_route_annotations.dart';
 import 'package:kata_note_flutter/presentation/auth/sign_in_page.dart';
+import 'package:kata_note_flutter/presentation/notes/notes_overview/notes_overview_page.dart';
 import 'package:kata_note_flutter/presentation/splash/splash_page.dart';
 
 @MaterialAutoRouter(
@@ -7,6 +8,7 @@ import 'package:kata_note_flutter/presentation/splash/splash_page.dart';
   routes: [
     MaterialRoute(page: SplashPage, initial: true),
     MaterialRoute(page: SignInPage),
+    MaterialRoute(page: NotesOverviewPage),
   ],
 )
 class $Router {}
